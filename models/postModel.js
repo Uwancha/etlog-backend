1a2a114a9c62f851d64815161fe6785a445d3ff7
Uwancha <sito2alemu@gmail.com>
2024-01-05 02:24:25 +0300

    feat: Define Post model Define post model to represent the structure of a post in the application. The post containes the content of the post and reference to the user wo created it

---
+++ models/postModel.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+/**
+ * PostSchema represents the structure of a post in the application.
+ * It includes the title, body, cover image and the user who authored it.
+ * Validation is implemented to ensure the existence of referenced user.
+ */
+
+const PostSchema = new Schema(
+    {
+        title: {
+            type: String, 
+            required: true, 
+            maxLength: 200
+        }, 
+        body: {
+            type: String, 
+            required: true
+        },
+        coverImage: {
+            type: String,
+            required: true
+        },
+
+        // The user who authored the post (referenced by their ObjectId)
+        user: {
+            type: Schema.Types.ObjectId, 
+            ref: "User",
+            required: true,
+
+            // Validation ensures that the referenced user exists
+            validate: {
+                validator: async (userId) => {
+                    // Checks if the user with given id exists
+                    const user = await mongoose.models.User.findById(userId);
+                    return user !== null
+                },
+                message: "User does not exist"
+            }
+        }
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
+
