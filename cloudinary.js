f3d1b15499917f4c1fe897210ea277dece518c3a
Uwancha <sito2alemu@gmail.com>
2024-01-11 22:17:53 +0300

    feat: Configure Cloudinary for image uploads

---
+++ cloudinary.js
@@ -0,0 +1,12 @@
+import { v2 as cloudinaryV2 } from 'cloudinary';
+
+// Configure Cloudinary
+cloudinaryV2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
+
+export { cloudinary, cloudinaryV2 };
+
