82f35f4f866f455266bcd2456d8a5b540e104953
Uwancha <sito2alemu@gmail.com>
2024-01-31 22:06:05 +0300

    chore: Fix typo in passport.js

--- utils/passport.js
+++ utils/passport.js
@@ -17,7 +17,7 @@ passport.use(
                 const isMatch = await bcrypt.compare(password, user.password);
 
                 if (!isMatch) {
-                    return done(null, false, {message: "Incorrect email"});
+                    return done(null, false, {message: "Incorrect password"});
                 }
 
                 return done(null, user);
